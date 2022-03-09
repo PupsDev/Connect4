0c7aade48da251b8f5e2ede42b9749a956572fbd
PupsDev <Control3d@gmail.com>
2022-03-09 11:03:47 +0100

    grille ok

--- main.cpp
+++ main.cpp
@@ -1,10 +1,107 @@
+#include <iostream>
 #include <SDL2/SDL.h>
+
+int SDL_RenderDrawCircle(SDL_Renderer * renderer, int x, int y, int radius)
+{
+    int offsetx, offsety, d;
+    int status;
+
+    // CHECK_RENDERER_MAGIC(renderer, -1);
+
+    offsetx = 0;
+    offsety = radius;
+    d = radius -1;
+    status = 0;
+
+    while (offsety >= offsetx) {
+        status += SDL_RenderDrawPoint(renderer, x + offsetx, y + offsety);
+        status += SDL_RenderDrawPoint(renderer, x + offsety, y + offsetx);
+        status += SDL_RenderDrawPoint(renderer, x - offsetx, y + offsety);
+        status += SDL_RenderDrawPoint(renderer, x - offsety, y + offsetx);
+        status += SDL_RenderDrawPoint(renderer, x + offsetx, y - offsety);
+        status += SDL_RenderDrawPoint(renderer, x + offsety, y - offsetx);
+        status += SDL_RenderDrawPoint(renderer, x - offsetx, y - offsety);
+        status += SDL_RenderDrawPoint(renderer, x - offsety, y - offsetx);
+
+        if (status < 0) {
+            status = -1;
+            break;
+        }
+
+        if (d >= 2*offsetx) {
+            d -= 2*offsetx + 1;
+            offsetx +=1;
+        }
+        else if (d < 2 * (radius - offsety)) {
+            d += 2 * offsety - 1;
+            offsety -= 1;
+        }
+        else {
+            d += 2 * (offsety - offsetx - 1);
+            offsety -= 1;
+            offsetx += 1;
+        }
+    }
+
+    return status;
+}
+
+
+int SDL_RenderFillCircle(SDL_Renderer * renderer, int x, int y, int radius)
+{
+    int offsetx, offsety, d;
+    int status;
+
+    // CHECK_RENDERER_MAGIC(renderer, -1);
+
+    offsetx = 0;
+    offsety = radius;
+    d = radius -1;
+    status = 0;
+
+    while (offsety >= offsetx) {
+
+        status += SDL_RenderDrawLine(renderer, x - offsety, y + offsetx,
+                                     x + offsety, y + offsetx);
+        status += SDL_RenderDrawLine(renderer, x - offsetx, y + offsety,
+                                     x + offsetx, y + offsety);
+        status += SDL_RenderDrawLine(renderer, x - offsetx, y - offsety,
+                                     x + offsetx, y - offsety);
+        status += SDL_RenderDrawLine(renderer, x - offsety, y - offsetx,
+                                     x + offsety, y - offsetx);
+
+        if (status < 0) {
+            status = -1;
+            break;
+        }
+
+        if (d >= 2*offsetx) {
+            d -= 2*offsetx + 1;
+            offsetx +=1;
+        }
+        else if (d < 2 * (radius - offsety)) {
+            d += 2 * offsety - 1;
+            offsety -= 1;
+        }
+        else {
+            d += 2 * (offsety - offsetx - 1);
+            offsety -= 1;
+            offsetx += 1;
+        }
+    }
+
+    return status;
+}
 
 int main()
 {
-    int grid_cell_size = 36;
-    int grid_width = 29;
-    int grid_height = 23;
+    int grid_cell_size = 160;
+    int offset =20;
+    int grid_cell_size_2=grid_cell_size/2 - offset;
+    int grid_width = 7;
+    int grid_height = 6;
+
+    int colonne_selected;
 
     // + 1 so that the last grid lines fit in the screen.
     int window_width = (grid_width * grid_cell_size) + 1;
@@ -24,9 +121,12 @@ int main()
                                   grid_cell_size};
 
     // Dark theme.
-    SDL_Color grid_background = {22, 22, 22, 255}; // Barely Black
-    SDL_Color grid_line_color = {44, 44, 44, 255}; // Dark grey
-    SDL_Color grid_cursor_ghost_color = {44, 44, 44, 255};
+    // SDL_Color grid_background = {22, 22, 22, 255}; // Barely Black
+    SDL_Color grid_background = {2, 43, 220, 255}; // Barely Blue
+    // SDL_Color grid_line_color = {44, 44, 44, 255}; // Dark grey
+    SDL_Color grid_line_color = {255, 255, 255, 255}; // White
+    // SDL_Color grid_cursor_ghost_color = {44, 44, 44, 255};
+    SDL_Color grid_cursor_ghost_color = {200, 200, 200, 255}; //
     SDL_Color grid_cursor_color = {255, 255, 255, 255}; // White
 
     // Light Theme.
@@ -83,6 +183,8 @@ int main()
             case SDL_MOUSEBUTTONDOWN:
                 grid_cursor.x = (event.motion.x / grid_cell_size) * grid_cell_size;
                 grid_cursor.y = (event.motion.y / grid_cell_size) * grid_cell_size;
+                // std::cout << "("<< grid_cursor.y/grid_cell_size <<", "<< grid_cursor.x/grid_cell_size <<")" << std::endl;
+                colonne_selected = grid_cursor.x/grid_cell_size;
                 break;
             case SDL_MOUSEMOTION:
                 grid_cursor_ghost.x = (event.motion.x / grid_cell_size) * grid_cell_size;
@@ -109,18 +211,28 @@ int main()
         SDL_RenderClear(renderer);
 
         // Draw grid lines.
-        SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
-                               grid_line_color.b, grid_line_color.a);
+         SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
+                                grid_line_color.b, grid_line_color.a);
+        
 
-        for (int x = 0; x < 1 + grid_width * grid_cell_size;
-             x += grid_cell_size) {
-            SDL_RenderDrawLine(renderer, x, 0, x, window_height);
+
+        // for (int x = 0; x < 1 + grid_width * grid_cell_size;
+        //      x += grid_cell_size) {
+        //     SDL_RenderDrawLine(renderer, x, 0, x, window_height);
+        // }
+
+        // for (int y = 0; y < 1 + grid_height * grid_cell_size;
+        //      y += grid_cell_size) {
+        //     SDL_RenderDrawLine(renderer, 0, y, window_width, y);
+        // }
+
+        for (int x = grid_cell_size_2 + offset; x < grid_width*grid_cell_size; x += grid_cell_size) {
+            for (int y = grid_cell_size_2 + offset; y < grid_width*grid_cell_size; y += grid_cell_size) {
+                SDL_RenderDrawCircle(renderer, y, x, grid_cell_size_2);
+                SDL_RenderFillCircle(renderer, y, x, grid_cell_size_2);
+            }
         }
 
-        for (int y = 0; y < 1 + grid_height * grid_cell_size;
-             y += grid_cell_size) {
-            SDL_RenderDrawLine(renderer, 0, y, window_width, y);
-        }
 
         // Draw grid ghost cursor.
         if (mouse_active && mouse_hover) {
@@ -128,14 +240,17 @@ int main()
                                    grid_cursor_ghost_color.g,
                                    grid_cursor_ghost_color.b,
                                    grid_cursor_ghost_color.a);
-            SDL_RenderFillRect(renderer, &grid_cursor_ghost);
+            // SDL_RenderFillRect(renderer, &grid_cursor_ghost);
+            SDL_RenderDrawCircle(renderer, grid_cursor_ghost.x + grid_cell_size_2 + offset, grid_cursor_ghost.y+ grid_cell_size_2+ offset, grid_cell_size_2);
+
+            SDL_RenderFillCircle(renderer, grid_cursor_ghost.x + grid_cell_size_2 + offset, grid_cursor_ghost.y+ grid_cell_size_2+ offset, grid_cell_size_2);
         }
 
         // Draw grid cursor.
-        SDL_SetRenderDrawColor(renderer, grid_cursor_color.r,
-                               grid_cursor_color.g, grid_cursor_color.b,
-                               grid_cursor_color.a);
-        SDL_RenderFillRect(renderer, &grid_cursor);
+        // SDL_SetRenderDrawColor(renderer, grid_cursor_color.r,
+        //                        grid_cursor_color.g, grid_cursor_color.b,
+        //                        grid_cursor_color.a);
+        // SDL_RenderFillRect(renderer, &grid_cursor);
 
         SDL_RenderPresent(renderer);
     }
